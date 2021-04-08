bdeb9814c4e685cffaf815b1a8563a33ea9889bc
dhananjay.mohnot <dhananjay.mohnot@zinier.com>
2021-04-08 09:19:37 +0530

    Table View code is created but I'm unable to test it. The app is blocked to be run in the Simulator by Cortex XDR and reported as a malware (weird).

--- Table View Simple Example/ViewController.swift
+++ Table View Simple Example/ViewController.swift
@@ -8,13 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var items: [String] = ["We", "heart", "Swift.", "Do", "you", "as", "well"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "cell")!
+        
+        cell.textLabel?.text = self.items[indexPath.row]
+        
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("didSelectRowAt: \(indexPath.row)")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
-
-
 }
-
